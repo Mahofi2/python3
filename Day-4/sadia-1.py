6091aaeb60e4e562d2a6a2d1a00f9090366a4531
mahofi2 <mahofiadiyan232@gmail.com>
2023-10-02 18:29:58 +0600

    finding leap year

--- Day-4/sadia-1.py
+++ Day-4/sadia-1.py
@@ -4,9 +4,9 @@ year = int (input("Enter a year"))
 
 if year % 4 == 0 :
     print ("This is leap year")
-if year % 100 == 0 :
+elif year % 100 == 0 :
     print ("This is leap year")
-if year % 400 == 0 :
+elif year % 400 == 0 :
     print ("This is leap year")
 else :
     print ("Normal year")
